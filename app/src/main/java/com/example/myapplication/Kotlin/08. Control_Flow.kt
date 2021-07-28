54f9a0dc2f34a4a320bb708f9b31c0245c70df0a
kh-min7 <rudgh2879@naver.com>
2021-07-28 23:35:36 +0900

    operator, Control_Flow

---
+++ app/src/main/java/com/example/myapplication/Kotlin/08. Control_Flow.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication.Kotlin
+
+// 08. 제어 흐름
+// - if, else
+
+fun main(args:Array<String>){
+    val a: Int = 5
+    val b: Int = 10
+
+    // if/else 사용하는 방법 (1)
+    if (a > b){
+        println("a 가 b 보다 크다")
+    }else {
+        println("a 가 b 보다 작다")
+    }
+
+    // if/else 사용하는 방법 (2)
+    if (a > b){
+        println("a 가 b 보다 크다")
+    }
+
+    // if/else/else if 사용하는 방법
+    if (a > b){
+        println("a 가 b 보다 크다")
+    }else if (a < b){
+        println("a 가 b 보다 작다")
+    }else{
+        println("a 와 b 는 같다")
+    }
+
+    // 값을 리턴하는 if 사용 방법 (1)
+    val max = if(a > b){
+        a // 5
+    }else{
+        b // 10
+    }
+    println()
+    println(max)
+
+    // 값을 리턴하는 if 사용 방법 (2)
+    val maxx = if (a > b) a else b
+    println()
+    println(maxx)
+}
