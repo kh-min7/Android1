c90e3b28433112ffdd532c673b80e07a09754b5e
kh-min7 <rudgh2879@naver.com>
2021-08-02 23:07:02 +0900

    Class

--- app/src/main/java/com/example/myapplication/Kotlin/17. Midterm.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/17. Midterm.kt
@@ -1,56 +1,56 @@
 package com.example.myapplication.Kotlin
 
-fun main(array: Array<String>) {
-    first()
-    second(80)
-    third(52)
-    gugudan()
-}
-
-fun first(){
-    val first_list = mutableListOf<Int>()
-    val second_list = mutableListOf<Boolean>()
-
-    for (i in 0..9){
-        first_list.add(i)
-    }
-
-    for (i in 0..9){
-        if (first_list[i] % 2 == 0){
-            second_list.add(first_list[i] % 2 == 0)
-        }else{
-            second_list.add(first_list[i] % 2 == 0)
-        }
-    }
-
-    println(first_list)
-    println(second_list)
-}
-
-fun second(a: Int){
-    when(a){
-        in 80..90 -> println("A")
-        in 70..79 -> println("B")
-        in 60..69 -> println("C")
-        in 0..59 -> println("F")
-
-    }
-}
-
-fun third(a:Int){
-    val T = a/10
-    val t = a%10
-    println(T+t)
-}
-
-fun gugudan(){
-    for (i in 1..9){
-        for (j in 1..9){
-            val ans = i*j
-            println("$i * $j = $ans")
-        }
-    }
-}
+//fun main(array: Array<String>) {
+//    first()
+//    second(80)
+//    third(52)
+//    gugudan()
+//}
+//
+//fun first(){
+//    val first_list = mutableListOf<Int>()
+//    val second_list = mutableListOf<Boolean>()
+//
+//    for (i in 0..9){
+//        first_list.add(i)
+//    }
+//
+//    for (i in 0..9){
+//        if (first_list[i] % 2 == 0){
+//            second_list.add(first_list[i] % 2 == 0)
+//        }else{
+//            second_list.add(first_list[i] % 2 == 0)
+//        }
+//    }
+//
+//    println(first_list)
+//    println(second_list)
+//}
+//
+//fun second(a: Int){
+//    when(a){
+//        in 80..90 -> println("A")
+//        in 70..79 -> println("B")
+//        in 60..69 -> println("C")
+//        in 0..59 -> println("F")
+//
+//    }
+//}
+//
+//fun third(a:Int){
+//    val T = a/10
+//    val t = a%10
+//    println(T+t)
+//}
+//
+//fun gugudan(){
+//    for (i in 1..9){
+//        for (j in 1..9){
+//            val ans = i*j
+//            println("$i * $j = $ans")
+//        }
+//    }
+//}
 
 // 강사 해설
 
---
+++ app/src/main/java/com/example/myapplication/Kotlin/18. Class.kt
@@ -0,0 +1,157 @@
+package com.example.myapplication.Kotlin
+
+// 18. Class
+// OOP -> Object Oriented Programing (객체지향 프로그래밍)
+
+// 객체란 뭘까?
+// - 이름이 있는 모든 것
+
+// 절차지향 프로그래밍 문제 해결 방법
+// - 코드를 위에서 부터 아래로 실행을 하면 문제가 해결된다
+
+// 객체지향 프로그래밍 문제 해결 방법
+// - 객체를 만들어서 객체에게 일을 시켜서 문제를 해결한다
+// - 선수, 심판, 경기장, 관중 -> 축구 게임
+
+//  객체를 만드는 방법
+// - 설명서가 있어야 한다
+
+fun main(array: Array<String>){
+
+    // 클래스(설명서)를 통해서 실체를 만드는 방법
+    // -> 인스턴스화 -> 인스턴스(객체)
+    Car("v8 engine", "big")
+
+    val bigCar = Car("v8 engine", "big")
+
+    // 우리가 만든 클래스(설명서)는 자료형이 된다
+    val bigCar1 : Car = Car("v8 engine", "big")
+
+    val superCar : SuperCar = SuperCar("good engine", "big", "white")
+
+    // 인스턴스가 가지고 있는 기능을 사용하는 방법
+    val runableCar : RunableCar = RunableCar("simple engine", "short body")
+    // RunableCar.ride() -> 불가능
+    runableCar.ride()
+    runableCar.navi("부산")
+    runableCar.drive()
+
+    // 인스턴스의 멤버 변수에 접근하는 방법
+    val runableCar2 : RunableCar2 = RunableCar2("nice engine", "long body")
+    println(runableCar2.body)
+    println(runableCar2.engine)
+
+    // 오버로딩
+    val testClass = TestClass()
+    testClass.test(1)
+    testClass.test(1,2)
+
+}
+
+// 클래스(설명서) 만드는 방법 (1)
+class Car(var engine : String, var body: String){
+
+}
+
+
+// 클래스(설명서) 만드는 방법 (2)
+class SuperCar{
+    var engine : String
+    var body : String
+    var door : String
+
+    constructor(engine : String, body: String, door: String){
+
+        this.engine = engine
+        this.body = body
+        this.door = door
+
+    }
+}
+
+
+// 클래스(설명서) 만드는 방법 (3) -> 1번 방법의 확장
+// 추가 인스턴스 변수는 따로 안에 적기
+// constructor -> 생성자
+class Car1 constructor(engine: String, body: String){
+    var door : String =""
+
+    // 생성자
+    constructor(engine:String, body:String, door:String) :this(engine,body) {
+        this.door = door
+    }
+}
+
+
+// 클래스(설명서) 만드는 방법 (4) -> 2번 방법의 확장
+class Car2{
+    var engine: String = ""
+    var body : String = ""
+    var door : String = ""
+
+    // 꼭 필요한 변수들
+    constructor(engine : String, body: String){
+        this.engine = engine
+        this.body = body
+    }
+
+    // 추가적인 변수가 있을 때때
+   constructor(engine:String, body:String, door:String){
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+}
+
+class RunableCar(engine: String, body: String){
+
+    fun ride(){
+        println("탑승 하였습니다")
+    }
+
+    fun drive(){
+        println("달립니다!")
+    }
+
+    fun navi(destination: String){
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+}
+
+class RunableCar2{
+    var engine: String
+    var body: String
+
+    constructor(engine: String, body: String){
+        this.engine = engine
+        this.body = body
+    }
+
+    init{
+        // 초기 셋팅을 할 때 유용하다
+        println("RunableCar2가 만들어 졌습니다")
+    }
+
+    fun ride(){
+        println("탑승 하였습니다")
+    }
+
+    fun drive(){
+        println("달립니다!")
+    }
+
+    fun navi(destination: String){
+        println("$destination 으로 목적지가 설정되었습니다")
+    }
+}
+
+// 오버로딩
+// -> 이름이 같지만 받는 파라미터가 다른 함수
+class TestClass(){
+    fun test(a: Int){
+        println("up")
+    }
+    fun test(a: Int, b: Int){
+        println("down")
+    }
+}
