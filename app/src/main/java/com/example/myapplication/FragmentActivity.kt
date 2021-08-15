2c5ffbf8456495420cab6a85abc89b50b67503bb
kh-min7 <rudgh2879@naver.com>
2021-08-15 18:56:41 +0900

    2021-08-15

---
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -0,0 +1,57 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
+
+class FragmentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        Log.d("life_cycle", "onCreate")
+
+
+        button.setOnClickListener{
+            // fragment를 동적으로 작동하는 방법
+            val fragmentOne: FragmentOne = FragmentOne()
+            val framentManager: FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            val fragmentTransaction = framentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+            // 끝을 내는 방법
+            // commit       -> 시간 될 때 해 (좀 더 안정적)
+            // commitnow    -> 지금 당장해
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle", "onDestroy")
+    }
+}
--- app/src/main/java/com/example/myapplication/Intent1.kt
+++ app/src/main/java/com/example/myapplication/Intent1.kt
@@ -22,17 +22,17 @@ class Intent1 : AppCompatActivity() {
 //            startActivity(intent)
 
 
-//            val intent2 = Intent(this@Intent1, Intent2::class.java)
-//            // apply
-//            intent2.apply{
-//                this.putExtra("number1", 3)
-//                this.putExtra("number2", 4)
-//            }
-//            // intent2에서 전달한거만 받음
-//            // startActivity(intent2)
-//
-//            // 리턴을 받는 요청
-//            startActivityForResult(intent2, 200)
+            val intent2 = Intent(this@Intent1, Intent2::class.java)
+            // apply
+            intent2.apply{
+                this.putExtra("number1", 3)
+                this.putExtra("number2", 4)
+            }
+            // intent2에서 전달한거만 받음
+            // startActivity(intent2)
+
+            // 리턴을 받는 요청
+            startActivityForResult(intent2, 200)
 
             // 암시적 인텐트
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
